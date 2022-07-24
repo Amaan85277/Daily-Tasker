fa5ec9b0f5ca5173d35b9d782c2bd6414a3dedf2
2Dfax <91450039+Amaan85277@users.noreply.github.com>
2022-07-24 22:21:27 +0530

    Add files via upload

---
+++ component/AddListModal.js
@@ -0,0 +1,131 @@
+import {
+  Text,
+  StyleSheet,
+  View,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import React, { Component, useState } from "react";
+import { AntDesign } from "react-native-vector-icons";
+
+import colors from "../color";
+import tempData from "../tempData";
+
+export default class AddListModal extends Component {
+  boxesColors = [
+    "#5CD859",
+    "#24A6D9",
+    "#595BD9",
+    "#8022D9",
+    "#D159D8",
+    "#D85963",
+    "#D88559",
+  ];
+
+  state = {
+    name: "",
+    color: this.boxesColors[0],
+  };
+
+  createTodo = () => {
+    const { name, color } = this.state;
+
+    const list = { name, color };
+
+    this.props.addList(list);
+
+    this.setState({ name: "" });
+    this.props.closeModal();
+  };
+
+  renderColors() {
+    return this.boxesColors.map((color) => {
+      return (
+        <TouchableOpacity
+          key={color}
+          style={[styles.colorSelect, { backgroundColor: color }]}
+          onPress={() => this.setState({ color })}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <TouchableOpacity
+          style={{ position: "absolute", top: 24, right: 30 }}
+          onPress={this.props.closeModal}
+        >
+          <AntDesign name="close" size={24} color={colors.black} />
+        </TouchableOpacity>
+
+        <View style={{ alignSelf: "stretch", marginHorizontal: 32 }}>
+          <Text style={styles.title}>Create Task</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Tasks Heading here..."
+            onChangeText={(text) => this.setState({ name: text })}
+          />
+
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginTop: 14,
+            }}
+          >
+            {this.renderColors()}
+          </View>
+
+          <TouchableOpacity
+            style={[styles.create, { backgroundColor: this.state.color }]}
+            onPress={this.createTodo}
+          >
+            <Text style={{ color: colors.white, fontWeight: "600" }}>
+              Create!
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "800",
+    color: colors.black,
+    alignSelf: "center",
+    marginBottom: "30%",
+  },
+  input: {
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: colors.blue,
+    borderRadius: 6,
+    height: 50,
+    marginTop: 8,
+    paddingHorizontal: 16,
+    fontSize: 18,
+  },
+  create: {
+    marginTop: 24,
+    height: 50,
+    borderRadius: 6,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  colorSelect: {
+    width: 30,
+    height: 30,
+    borderRadius: 4,
+  },
+});
